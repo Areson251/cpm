b7acbd0cb138b54dab212a3463a6e9ad08cfdb9d
Areson251 <unicorn5596@gmail.com>
2022-10-10 21:38:19 +0400

    added gray convolution

--- comparison.py
+++ comparison.py
@@ -1,13 +1,16 @@
+# from turtle import width
+from ctypes.wintypes import SHORT
 import cv2
 import numpy as np
 
 IMAGE_1_PATH = 'photos/1.png'
 IMAGE_2_PATH = 'photos/3.png'
 
+SHAPE = 10
+
 
 def count_difference(image1, image2):
-    width = round(image1.shape[1] / image2.shape[1])
-    height = round(image1.shape[0] / image2.shape[0])
+    width, height = count_shapes(image1, image2)
 
     image_pixels = []
 
@@ -26,8 +29,30 @@ def count_difference(image1, image2):
     return np.array(image_pixels)
 
 
-#TODO: rename function to correct 
-def create_hist(pixels):
+def create_convolution(image1, shape):
+    image_pixels = []
+    width = round(image1.shape[1] / shape)
+    height = round(image1.shape[0] / shape)
+
+    for i_num in range(height):
+        pixels_row = []
+        for j_num in range(width):
+            for i in range(shape):
+                for j in range(shape):
+                    pixel = image1.item((i_num*shape + i, j_num*shape + j))
+            pixels_row.append(round(pixel))      
+        image_pixels.append(pixels_row)  
+
+    return np.array(image_pixels)
+
+
+def count_shapes(image1, image2):
+    width = round(image1.shape[1] / image2.shape[1])
+    height = round(image1.shape[0] / image2.shape[0])
+    return width, height
+
+
+def create_image(pixels):
     avarage = int(np.sum(pixels) / (pixels.shape[0] * pixels.shape[1]))
     pixels[pixels < avarage] = 0
     
@@ -39,11 +64,13 @@ if __name__ == "__main__":
     image1 = cv2.imread(IMAGE_1_PATH,0)
     image2 = cv2.imread(IMAGE_2_PATH,0)
 
-    pixels = count_difference(image1, image2)
+    # pixels = count_difference(image1, image2)
+    pixels = create_convolution(image1, SHAPE)
 
-    pixels = create_hist(pixels)
+    pixels = create_image(pixels)
     
     # show image
-    cv2.imshow('result',np.uint8(pixels))
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # cv2.imshow('result',np.uint8(pixels))
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    cv2.imwrite(f'photos/result_{SHAPE}.png', pixels)
